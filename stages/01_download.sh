fb6558172e38fa8ebf98eeda434c4be9fd24c47f
nibafanfan <nibafanfan.gmail>
2024-03-19 11:26:46 -0400

    cleaned code

--- stages/01_download.sh
+++ stages/01_download.sh
@@ -1,25 +1,3 @@
 #!/usr/bin/env bash
-
-# Script to download data from Figshare
-
-# Define the Figshare URL for the dataset
-figshare_url="https://ndownloader.figshare.com/articles/5104873/versions/1"
-
-# Get local path
-localpath=$(pwd)
-echo "Local path: $localpath"
-
-# Create the download directory
-downloadpath="$localpath/download"
-echo "Download path: $downloadpath"
-mkdir -p "$downloadpath"
-
-# Change to the download directory
-cd $downloadpath;
-
-# Download the file from Figshare
-echo "Downloading data from Figshare..."
-wget -nc "$figshare_url" -O chemical_reactions.zip
-
-
-echo "Download complete."
+mkdir -p download
+wget -nc "https://ndownloader.figshare.com/articles/5104873/versions/1" -O download/chemical_reactions.zip
