6f4522b65978621ba7030b860326748329e1e2b2
nibafanfan <nibafanfan.gmail>
2024-03-20 10:33:21 -0400

    USPTO_chem_reaction is functional

--- stages/03_build.py
+++ stages/03_build.py
@@ -1,26 +1,22 @@
-# TODO: output to brick with two df parquets, change yaml too.
-# TODO: get the reaction product thing working.
-# TODO: blog post
 import os
 import pandas as pd
 from tqdm import tqdm
+import glob
+import pathlib
 
-def process_files(start_path, output_dir):
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
+brick = pathlib.Path('brick')
+brick.mkdir(exist_ok=True)
+rsmi = glob.glob('raw/**/*.rsmi', recursive=True)
 
-    for dirpath, dirnames, filenames in os.walk(start_path):
-        for filename in tqdm(filenames, desc="Processing files", unit="file"):
-            if filename.endswith('.rsmi'):
-                file_path = os.path.join(dirpath, filename)
-                try:
-                    df = pd.read_csv(file_path, sep='\t', dtype={'ParagraphNum': str})
-                    output_filename = os.path.join(output_dir, filename.replace('.rsmi', '.parquet'))
-                    df.to_parquet(output_filename)
-                    print(f"Data saved to {output_filename}")
-                except pd.errors.ParserError as e:
-                    print(f"Error reading file {file_path}: {e}")
+for path in tqdm(rsmi, desc="Processing files", unit="file"):
+    df = pd.read_csv(path, sep='\t', dtype={'ParagraphNum': str})
+    out = brick / os.path.basename(path).replace('.rsmi', '.parquet')
+    df.to_parquet(out)
+    
+# Get the number of lines of first rsmi file
+numlines = sum(1 for line in open(rsmi[-1]))
+assert (numlines - 1) == len(df), f"Number of lines in {rsmi[-1]} does not match the number of rows in the DataFrame"
 
-process_files('raw', 'brick')
+    
 
     
